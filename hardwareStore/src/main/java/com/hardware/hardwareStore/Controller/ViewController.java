13631139f3a966d5136cbb002e11f1496f10c997
Sebastian Rojas Gonzalez <rojasgonzales0714@gmail.com>
2025-08-09 02:11:09 -0500

    fix

--- hardwareStore/src/main/java/com/hardware/hardwareStore/Controller/ViewController.java
+++ hardwareStore/src/main/java/com/hardware/hardwareStore/Controller/ViewController.java
@@ -4,18 +4,14 @@ import com.hardware.hardwareStore.Repository.*;
 import com.hardware.hardwareStore.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-<<<<<<< Updated upstream
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import java.util.Date;
 import java.util.List;
-=======
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
->>>>>>> Stashed changes
 
 @Controller
 public class ViewController {
