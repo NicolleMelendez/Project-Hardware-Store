35c9166a9a3eacaabbd701e6c2b2f8878b03eac9
Nicolle Melendez <nicollev2020@gmail.com>
2025-08-08 22:57:17 -0500

    FIX vista
    client queda listo con crud

--- hardwareStore/src/main/java/com/hardware/hardwareStore/Controller/ViewController.java
+++ hardwareStore/src/main/java/com/hardware/hardwareStore/Controller/ViewController.java
@@ -2,26 +2,53 @@ package com.hardware.hardwareStore.Controller;
 
 
 
+import com.hardware.hardwareStore.Repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import com.hardware.hardwareStore.model.Client;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class ViewController {
 
+    @Autowired
+    private ClientRepository clientRepository;
+
     @GetMapping("/")
     public String home() {
         return "index";
     }
 
-
     @GetMapping("/client")
     public String clientPage(Model model) {
-        model.addAttribute("content", "client/index :: content");
+        model.addAttribute("clients", clientRepository.findAll());
+        model.addAttribute("client", new Client()); // para el modal
         return "client/index";
+    }
+
+    @PostMapping("/save")
+    public String saveClient(@ModelAttribute Client client) {
+        clientRepository.save(client);
+        return "redirect:/client"; // aquí el cambio
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteClient(@PathVariable Long id) {
+        clientRepository.deleteById(id);
+        return "redirect:/client"; // aquí el cambio
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editClient(@PathVariable Long id, Model model) {
+        model.addAttribute("clients", clientRepository.findAll());
+        model.addAttribute("client", clientRepository.findById(id).orElse(new Client()));
+        return "client/index"; // este está bien, no es redirect
     }
 
     @GetMapping("/employee")
