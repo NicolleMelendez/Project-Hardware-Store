f8b67a58c4070b9741a0b3a2068cb0ae56fec173
Sebastian Rojas Gonzalez <rojasgonzales0714@gmail.com>
2025-08-12 23:41:52 -0500

    Login y Register
    se implementa toda la lógica, funcionalidad, autenticación para el login y register

---
+++ hardwareStore/src/main/java/com/hardware/hardwareStore/Security/CustomUserDetails.java
@@ -0,0 +1,35 @@
+package com.hardware.hardwareStore.Security;
+
+import com.hardware.hardwareStore.model.Users;
+import com.hardware.hardwareStore.model.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Users user;
+
+    public CustomUserDetails(Users u) { this.user = u; }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRole() == null ?
+                java.util.List.of() :
+                java.util.List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().getName()));
+    }
+
+    @Override public String getPassword() { return user.getPassword(); }
+    @Override public String getUsername() { return user.getEmail(); }
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
+
+    public String getName() {
+        return user.getName();
+    }
+}
+
