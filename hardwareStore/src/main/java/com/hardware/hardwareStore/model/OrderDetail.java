1988c55738ca7a24345817103bead0a66272982e
Sebastian Rojas Gonzalez <rojasgonzales0714@gmail.com>
2025-08-26 19:20:04 -0500

    Vista Orderbuy y detalles

--- hardwareStore/src/main/java/com/hardware/hardwareStore/model/OrderDetail.java
+++ hardwareStore/src/main/java/com/hardware/hardwareStore/model/OrderDetail.java
@@ -15,14 +15,12 @@ public class OrderDetail {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY) // fetch = LAZY es una buena práctica
-    // @MapsId("orderBuy") // <-- SE ELIMINA ESTA LÍNEA
-    @JoinColumn(name = "id_order_buy", nullable = false) // 'nullable = false' asegura integridad
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_order_buy", nullable = false)
     @JsonBackReference
     private OrderBuy orderBuy;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    // @MapsId("inventory") // <-- SE ELIMINA ESTA LÍNEA
     @JoinColumn(name = "id_inventory", nullable = false)
     private Inventory inventory;
 
